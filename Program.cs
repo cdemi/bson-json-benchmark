c5ede2fb6a5dc52127fd1a182f9d36d8d11f9866
Christopher Demicoli <christopher.demicoli@outlook.com>
2016-04-24 12:49:15 +0200

    Base Project

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSON_JSON_Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
