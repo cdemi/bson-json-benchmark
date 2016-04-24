af5e879a4690dd08443e39bade2beba3d98de1d9
Christopher Demicoli <christopher.demicoli@outlook.com>
2016-04-24 13:20:20 +0200

    Reading Sample Data from File

--- Program.cs
+++ Program.cs
@@ -1,15 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 
 namespace BSON_JSON_Benchmark
 {
     class Program
     {
+        static string BigJSON;
+        static string SmallJSON;
+
+        static MemoryStream BigBSON = new MemoryStream();
+        static MemoryStream SmallBSON = new MemoryStream();
         static void Main(string[] args)
         {
+            BigJSON = File.ReadAllText("App_Data\\JSON\\Big.json");
+            SmallJSON = File.ReadAllText("App_Data\\JSON\\Small.json");
+
+            using (var bigBsonFile = File.OpenRead("App_Data\\BSON\\Big.bson"))
+            {
+                bigBsonFile.CopyTo(BigBSON);
+            }
+            using (var smallBsonFile = File.OpenRead("App_Data\\BSON\\small.bson"))
+            {
+                smallBsonFile.CopyTo(SmallBSON);
+            }
         }
     }
 }
